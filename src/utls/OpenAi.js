8fc8bb7fca44f09a588be287b50058036a3de66b
kuldeepparmar <kuldeep07122002@gmail.com>
2025-04-20 18:50:19 +0530

    some change in .env

--- src/utls/OpenAi.js
+++ src/utls/OpenAi.js
@@ -1,8 +1,9 @@
 import OpenAI from 'openai';
 import { OPENAI_KEY } from './constant';
+ 
 
 const client = new OpenAI({
-  apiKey: [OPENAI_KEY], 
+  apiKey: [process.env.OPENAI_API_KEY], 
   dangerouslyAllowBrowser: true , 
 });
 export default client;
--- src/utls/firebase.js
+++ src/utls/firebase.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+ 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSVhHOZXO-s4BtKRkAo2TN7MrcdODO5kg",
+  apiKey: [process.env.FIREABASE_API_KEY],
   authDomain: "netflix-gpt-c1020.firebaseapp.com",
   projectId: "netflix-gpt-c1020",
   storageBucket: "netflix-gpt-c1020.appspot.com",
