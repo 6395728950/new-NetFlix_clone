d13ea80efa47fcc1906aeda9caf11293a4d4abc8
kuldeepparmar <kuldeep07122002@gmail.com>
2025-04-21 21:37:37 +0530

    some extra change in env file

--- src/utls/firebase.js
+++ src/utls/firebase.js
@@ -11,12 +11,12 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "netflix-gpt-c1020.firebaseapp.com",
-  projectId: "netflix-gpt-c1020",
-  storageBucket: "netflix-gpt-c1020.appspot.com",
-  messagingSenderId: "918885258912",
-  appId: "1:918885258912:web:b6e93c4acd7846728adbc5",
-  measurementId: "G-8PKT6LK5EQ"
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 // Initialize Firebase
